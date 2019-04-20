489ca36eae06034ae9c238ff925449b9b0e2f0c1
Juuso Tuononen <jutuon@outlook.com>
2019-04-20 11:04:33 +0300

    Add link to Intel graphics VGA register reference.

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 //! * <http://classiccomputers.info/down/IBM_PS2/documents/PS2_Hardware_Interface_Technical_Reference_May88.pdf>
 //!     * PDF page 388
 //! * <https://wiki.osdev.org/VGA_Hardware>
+//! * [Intel® OpenSource HD Graphics Programmer’s Reference Manual (PRM) Volume 3 Part 1: VGA and Extended VGA Registers (Ivy Bridge)](https://01.org/sites/default/files/documentation/ivb_ihd_os_vol3_part1_0.pdf)
 
 #![no_std]
 #![forbid(missing_debug_implementations, unsafe_code)]
